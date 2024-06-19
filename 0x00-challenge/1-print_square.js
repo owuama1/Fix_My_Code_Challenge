339277aa58af1acbc741afc9a89144c69b2761aa
owuama1 <kelechiowuama24@gmail.com>
2024-06-19 10:12:36 +0000

    edit

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,6 +6,7 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -13,16 +14,10 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-// Parse the size as a decimal number
-let size = parseInt(process.argv[2], 10);
+size = parseInt(process.argv[2], 10)
 
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid size\n");
-    process.exit(1);
-}
-
-for (let i = 0 ; i < size ; i++) {
-    for (let j = 0 ; j < size ; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
